e536d5f661b57f802c2c7d61638100db571dce03
yuting.fu <yuting.fu@yituishui.com>
2020-07-03 15:29:40 +0800

    修复下拉刷新bug

--- app/src/main/java/com/fyt/myapplication/mvvm/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/fyt/myapplication/mvvm/viewmodel/MainViewModel.kt
@@ -11,59 +11,64 @@ import com.fyt.myapplication.mvvm.repository.MainRepository
 import com.fyt.myapplication.mvvm.viewmodel.uistate.MainUiState
 import com.fyt.myapplication.mvvm.repository.bean.UserBean
 
-class MainViewModel(application: Application,
-                    mainRepository: MainRepository,
-                    responseErrorListener: IResponseErrorListener
-) : BaseViewModel<MainRepository, MainUiState>(application,mainRepository,responseErrorListener) {
+class MainViewModel(
+    application: Application,
+    mainRepository: MainRepository,
+    responseErrorListener: IResponseErrorListener
+) : BaseViewModel<MainRepository, MainUiState>(application, mainRepository, responseErrorListener) {
 
     private var mainUiState = MutableLiveData<MainUiState>(MainUiState())
     override fun getUiState(): LiveData<MainUiState> = mainUiState
 
-    private val userDataSource = object: ItemKeyedDataSource<Long,UserBean>(){
-        override fun loadInitial(params: LoadInitialParams<Long>, callback: LoadInitialCallback<UserBean>) {
-            apply(object : ResultCallBack<List<UserBean>> {
-                override suspend fun callBack(): BaseResult<List<UserBean>> {
-                    return mRepository.getUsers(1,params.requestedLoadSize)
-                }
-            },{
-                callback.onResult(it)
-                mainUiState.postNext {state->
-                    state.copy(referish = true,loadMode = false)
-                }
-            },{e ->
-                mResponseErrorListener.handleResponseError(e)
-                mainUiState.postNext {state->
-                    state.copy(referish = true,loadMode = false)
-                }
-            })
-        }
+    private fun createDataSource(): DataSource<Long, UserBean> {
+        return object : ItemKeyedDataSource<Long, UserBean>() {
+            override fun loadInitial(params: LoadInitialParams<Long>, callback: LoadInitialCallback<UserBean>) {
+                apply(object : ResultCallBack<List<UserBean>> {
+                    override suspend fun callBack(): BaseResult<List<UserBean>> {
+                        return mRepository.getUsers(1, params.requestedLoadSize)
+                    }
+                }, {
+                    callback.onResult(it)
+                    mainUiState.postNext { state ->
+                        state.copy(referish = true, loadMode = false)
+                    }
+                }, { e ->
+                    mResponseErrorListener.handleResponseError(e)
+                    mainUiState.postNext { state ->
+                        state.copy(referish = true, loadMode = false)
+                    }
+                })
+            }
 
-        override fun loadAfter(params: LoadParams<Long>, callback: LoadCallback<UserBean>) {
-            apply(object : ResultCallBack<List<UserBean>> {
-                override suspend fun callBack(): BaseResult<List<UserBean>> {
-                    return mRepository.getUsers(params.key,params.requestedLoadSize)
-                }
-            },{
-                callback.onResult(it)
-            },{e ->
-                mResponseErrorListener.handleResponseError(e)
-            })
-        }
+            override fun loadAfter(params: LoadParams<Long>, callback: LoadCallback<UserBean>) {
+                apply(object : ResultCallBack<List<UserBean>> {
+                    override suspend fun callBack(): BaseResult<List<UserBean>> {
+                        return mRepository.getUsers(params.key, params.requestedLoadSize)
+                    }
+                }, {
+                    callback.onResult(it)
+                }, { e ->
+                    mResponseErrorListener.handleResponseError(e)
+                })
+            }
 
-        override fun loadBefore(params: LoadParams<Long>, callback: LoadCallback<UserBean>) {
-        }
+            override fun loadBefore(params: LoadParams<Long>, callback: LoadCallback<UserBean>) {
+            }
 
-        override fun getKey(item: UserBean): Long {
-            return item.id
-        }
+            override fun getKey(item: UserBean): Long {
+                return item.id
+            }
 
+        }
     }
 
     val userLiveData =
-        LivePagedListBuilder(mRepository.createDataSourceFactory(userDataSource),
-            mRepository.createConfig()).build()
+        LivePagedListBuilder(mRepository.createDataSourceFactory(createDataSource()),
+            mRepository.createConfig())
+            .setInitialLoadKey(1)
+            .build()
 
-    fun onRefresh(){
+    fun onRefresh() {
         userLiveData.value!!.dataSource.invalidate()
     }
 
